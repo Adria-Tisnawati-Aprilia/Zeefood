63c34012d3a6d60c7503a838b6a6ea186aa13901
Adria-Tisnawati-Aprilia <adriatisnawatiaprilia@gmail.com>
2022-10-26 14:57:03 +0700

    Membuat Resto Vue

---
+++ zeefood/src/views/Foods.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <NavItem/>
+        <h2>Daftar Makanan</h2>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavItem from '@/components/NavItem.vue'
+
+export default {
+    name : 'FoodS',
+    components: {
+        NavItem
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ zeefood/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <NavItem/>
+    <div class="container">
+      <HeroF/>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavItem from '@/components/NavItem.vue'
+import HeroF from '@/components/Hero.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    NavItem,
+    HeroF
+  }
+}
+</script>
