b67b1df2b4a03a0457ec31c80d6cd47c17666c40
Adria-Tisnawati-Aprilia <adriatisnawatiaprilia@gmail.com>
2022-10-31 15:44:34 +0700

    Footer

--- zeefood/src/App.vue
+++ zeefood/src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app"> 
     <router-view/>
+    <FootEr/>
   </div>
 </template>
 
+<script>
+import FootEr from "@/components/Footer.vue"
+
+export default {
+  components : {
+    FootEr
+  }
+}
+</script>
