23ea811c479ee98718ccb6a4f654582e9426d43c
Adria-Tisnawati-Aprilia <adriatisnawatiaprilia@gmail.com>
2022-10-31 22:51:59 +0700

    Membuat halaman detail product

--- zeefood/src/views/FoodDetail.vue
+++ zeefood/src/views/FoodDetail.vue
@@ -1,11 +1,76 @@
 <template>
-    <h1>Food Detail {{ $route.params.id }}</h1>
+    <div class="food-detail">
+        <NavItem/>
+        <div class="container">
+        
+            <!-- Breadcrumb-->
+            <div class="row mt-5">
+                <div class="col">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item">
+                                <router-link to="/" class="text-dark">Home</router-link>
+                            </li>
+                            <li class="breadcrumb-item">
+                                <router-link to="/foods" class="text-dark">Food</router-link>
+                            </li>
+                            <li class="breadcrumb-item active" aria-current="page">Food Order</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+            
+            <div class="row mt-3">
+                <div class="col-md-6">
+                    <img :src="'../assets/images/'+ product.gambar" class="img-fluid shadow"/>
+                </div>
+                <div class="col-md-6">
+                    <h2><strong> {{ product.nama }}</strong></h2>
+                    <hr>
+                    <h4>Harga : <strong>Rp. {{ product.harga}}</strong></h4>
+                    <form class="mr-4">
+                        <div class="form-group">
+                            <label for="jumlah_pemesanan">Jumlah Pesan</label>
+                            <input type="number" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="keterangan">Keterangan</label>
+                            <textarea class="form-control" placeholder="Masukan keterangan"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-success"><b-icon-cart></b-icon-cart>Pesan</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import NavItem from '@/components/NavItem.vue';
+import axios from 'axios';
+
 export default {
-    name : 'FoodDetail'
-}
+    name : 'FoodDetail',
+    components: {
+        NavItem,
+    },
+    data() {
+        return {
+            product : {}
+        }
+    },
+    methods: {
+        setProduct(data) {
+            this.product = data
+        }
+    },
+    mounted() {
+        axios
+        .get("http://localhost:3000/products/"+this.$route.params.id)
+        .then((response) => this.setProduct(response.data))
+        .catch((error) => console.log(error))
+    },
+};
 </script>
 
 <style>
